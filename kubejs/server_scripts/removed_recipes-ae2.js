c5b8fa578d07ec9425d58adc1f57da0a2640c918
Pixel <scott.blatt.0b10@gmail.com>
2025-07-09 15:45:07 -0500

    base pack

---
+++ kubejs/server_scripts/removed_recipes-ae2.js
@@ -0,0 +1,92 @@
+ServerEvents.recipes(event => 
+	{
+
+
+		//AE2
+            event.remove({id: 'ae2:tools/certus_quartz_sword'})
+            event.remove({id: 'ae2:tools/certus_quartz_pickaxe'})
+            event.remove({id: 'ae2:tools/certus_quartz_axe'})
+            event.remove({id: 'ae2:tools/certus_quartz_spade'})
+            event.remove({id: 'ae2:tools/certus_quartz_hoe'})
+            event.remove({id: 'ae2:tools/nether_quartz_pickaxe'})
+            event.remove({id: 'ae2:tools/nether_quartz_axe'})
+            event.remove({id: 'ae2:tools/nether_quartz_spade'})
+            event.remove({id: 'ae2:tools/nether_quartz_hoe'})
+            event.remove({id: 'ae2:tools/nether_quartz_sword'})
+            event.remove({id: 'ae2:tools/fluix_pickaxe'})
+            event.remove({id: 'ae2:tools/fluix_axe'})
+            event.remove({id: 'ae2:tools/fluix_shovel'})
+            event.remove({id: 'ae2:tools/fluix_hoe'})
+            event.remove({id: 'ae2:tools/fluix_sword'})
+            event.remove({id: 'ae2:tools/certus_quartz_cutting_knife'})
+            event.remove({id: 'ae2:tools/nether_quartz_cutting_knife'})
+            event.remove({id: 'ae2:tools/misctools_entropy_manipulator'})
+            event.remove({id: 'ae2:tools/misctools_charged_staff'})
+            event.remove({id: 'ae2:tools/network_color_applicator'})
+            event.remove({id: 'ae2:tools/matter_cannon'})
+            event.remove({id: 'ae2:tools/network_memory_card'})
+            event.remove({id: 'ae2:network/crafting/patterns_blank'})
+            event.remove({id: 'ae2:materials/basiccard'})
+            event.remove({id: 'ae2:materials/advancedcard'})
+            event.remove({id: 'ae2:network/cells/item_cell_housing'})
+            event.remove({id: 'ae2:network/wireless_part'})
+            event.remove({id: 'ae2:network/wireless_booster'})
+            event.remove({id: 'ae2:network/cells/view_cell'})
+            event.remove({id: 'ae2:network/cells/item_storage_components_cell_1k_part'})
+            event.remove({id: 'ae2:network/cells/item_storage_cell_1k'})
+            event.remove({id: 'ae2:network/cells/item_storage_cell_4k'})
+            event.remove({id: 'ae2:network/cells/item_storage_cell_16k'})
+            event.remove({id: 'ae2:network/cells/item_storage_cell_64k'})
+            event.remove({id: 'ae2:network/cells/item_storage_cell_256k'})
+            event.remove({id: 'ae2:network/cells/spatial_storage_cell_2_cubed'})
+            event.remove({id: 'ae2:network/cells/spatial_storage_cell_16_cubed'})
+            event.remove({id: 'ae2:network/cells/spatial_storage_cell_128_cubed'})
+            event.remove({id: 'ae2:network/blocks/inscribers'})
+            event.remove({id: 'ae2:network/blocks/crystal_processing_charger'})
+            event.remove({id: 'ae2:network/blocks/quantum_ring'})
+            event.remove({id: 'ae2:network/blocks/spatial_io_port'})
+            event.remove({id: 'ae2:network/blocks/storage_drive'})
+            event.remove({id: 'ae2:network/blocks/storage_chest'})
+            event.remove({id: 'ae2:network/blocks/interfaces_interface'})
+            event.remove({id: 'ae2:network/blocks/cell_workbench'})
+            event.remove({id: 'ae2:network/blocks/io_port'})
+            event.remove({id: 'ae2:network/blocks/io_condenser'})
+            event.remove({id: 'ae2:network/blocks/energy_energy_acceptor'})
+            event.remove({id: 'ae2:network/crystal_resonance_generator'})
+            event.remove({id: 'ae2:network/blocks/energy_vibration_chamber'})
+            event.remove({id: 'ae2:network/blocks/crystal_processing_growth_accelerator'})
+            event.remove({id: 'ae2:network/crafting/cpu_crafting_unit'})
+            event.remove({id: 'ae2:network/blocks/pattern_providers_interface'})
+            event.remove({id: 'ae2:network/crafting/molecular_assembler'})
+            event.remove({id: 'ae2:network/blocks/spatial_anchor'})
+            event.remove({id: 'ae2:network/parts/panels_semi_dark_monitor'})
+            event.remove({id: 'ae2:network/parts/import_bus'})
+            event.remove({id: 'ae2:network/parts/export_bus'})
+            event.remove({id: 'ae2:network/parts/annihilation_plane_alt'})
+            event.remove({id: 'ae2:network/parts/annihilation_plane_alt2'})
+            event.remove({id: 'ae2:network/parts/formation_plane'})
+            event.remove({id: 'ae2:network/parts/formation_plane_alt'})
+            event.remove({id: 'ae2:network/parts/tunnels_me'})
+            event.remove({id: 'ae2:network/blocks/crank'})
+            event.remove({id: 'ae2:network/blocks/controller'})
+            event.remove({id: 'ae2:misc/chests_sky_stone'})
+            event.remove({id: 'ae2:misc/chests_smooth_sky_stone'})
+
+
+
+
+
+            event.remove({id: 'ae2:materials/cardspeed'})
+
+
+
+
+
+
+            event.remove({id: 'ae2:charger/meteorite_compass'})
+
+
+
+
+
+    })
