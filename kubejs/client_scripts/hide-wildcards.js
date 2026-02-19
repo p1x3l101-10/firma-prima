197ec866ef22bac0ea369befa817f7fddf78b7b1
Pixel <scott.blatt.0b10@gmail.com>
2026-02-19 16:39:48 -0600

    purge kubejs

--- kubejs/client_scripts/hide-wildcards.js
+++
@@ -1,3 +0,0 @@
-JEIEvents.removeEntries(event => {
-  event.remove("artisinal:barrel/lava_garbage_disposal");
-})
--- kubejs/server_scripts/ars_tfc.js
+++
@@ -1,4 +0,0 @@
-ServerEvents.recipes(event => {
-  event.remove({ output: 'ars_nouveau:archwood_planks' });
-	event.shapeless('4x ars_nouveau:archwood_planks', [Item.of('#tfc:saws').ignoreNBT(), '#forge:logs/archwood']).damageIngredient(0).keepIngredient('#tfc:saws');
-})
